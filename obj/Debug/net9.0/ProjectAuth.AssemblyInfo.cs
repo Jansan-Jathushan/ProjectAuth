b32df52247a1869c0c00f4b667b377065345a591
Jansan-Jathushan <jansanjathushan@gmail.com>
2025-04-21 18:29:14 +0530

    Token Expiry Test scuss so change it back to old code

--- obj/Debug/net9.0/ProjectAuth.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProjectAuth.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectAuth")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e43c575e263ad84831a4c81e90a0a27d38add21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e0e6454a0868ca74a97c8e05006aafd4efdab61")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectAuth")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectAuth")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
